820f1eddb370b9675158e0c1525ca633e487dd87
farhan <farhan@farhans-mbp.home>
2020-05-14 14:57:23 -0400

    made changes to account class

--- Bank/src/Account.java
+++ Bank/src/Account.java
@@ -1,7 +1,11 @@
 
 public interface Account {
+	//will add int amount to balance
 	public void deposit(int amount);
+	//will subtract int amount to balance
 	public void withdraw(int amount);
-	public int balance();
+	//return int balance
+	public int getbal();
+	//return information of the object
 	public String toString();
 }
